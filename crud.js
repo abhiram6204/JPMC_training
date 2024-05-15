93ee9ec30a6f23f6a8504829dc27e7efd01c5939
abhiram6204 <132203076+abhiram6204@users.noreply.github.com>
2024-05-15 15:34:45 +0530

    Add files via upload

--- crud.js
+++ crud.js
@@ -9,7 +9,9 @@ function getData()
             temp = '<tr><td>' + item.id + '</td>';
             temp+= '<td>' + item.name+ '</td>';
             temp+= '<td>' + item.branch + '</td>';
-            temp+= '<td>' + item.email + '</td></tr>';
+            temp+= '<td>' + item.email + '</td>';
+            temp+=`<td><button onclick="updateData(${item.id})">Update Data</button>`
+            temp+=`<button onclick="deleteData(${item.id})">Delete Data</button></td></tr>`
             tbody+=temp;
         }
         document.getElementById("tbody").innerHTML=tbody
@@ -17,45 +19,68 @@ function getData()
 )
 
 }
-function addData()
+function addData(Event)
 {
-    let id=prompt("Enter id to add")
-    let name=prompt("Enter name to add")
-    let branch=prompt("Enter branch to add")
-    let email=prompt("Enter email to add")
+    event.preventDefault(); // Prevent default form submission behavior
+    
+    let id=document.getElementById("id").value
+    let name=document.getElementById("name").value
+    let branch=document.getElementById("branch").value
+    let email=document.getElementById("email").value
+    
+    // Send POST request to add new user
     fetch("http://localhost:3000/users",{
         method:"POST",
-        body:JSON.stringify({
-         "id":id,
-         "name":name,
-         "branch":branch,
-         "email":email})
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            id,
+            name,
+            branch,
+            email
+        })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Failed to add user');
         }
-    )
-    .then(response=>response.json())
-    alert("data added")
-
+        return response.json();
+    })
+    .then(data => {
+        // Refresh data after successful addition
+        getData();
+    })
+    .catch(error => {
+        console.error('Error adding user:', error);
+        // Handle error
+    });
 }
-function deleteData()
+function deleteData(id1)
 {
-    let id1=prompt("Enter id to delete")
     fetch("http://localhost:3000/users/"+id1,{
         method:"DELETE"
     })
     .then(response=>response.json())
     .catch(err=>console.log(err))
+    getData()
 }
-function updateData()
+function updateData(id2)
 {
-    let id2=prompt("Enter id to update")
-    let email=prompt("Enter email to update")
-    fetch("http://localhost:3000/users/4",{
+    let id=document.getElementById("id").value
+    let name=document.getElementById("name").value
+    let branch=document.getElementById("branch").value
+    let email=document.getElementById("email").value
+    fetch(`http://localhost:3000/users/${id2}`,{
         method:"PATCH",
         body:JSON.stringify({
-            "id":id2,
+            "id":id,
+            "name":name,
+            "branch":branch,
             "email":email
         })
     })
     .then(response=>response.json())
     .then(data=>console.log(data))
+    getData()
 }
